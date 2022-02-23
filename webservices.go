9459bbf782a1dbc5888b2dc459bee0c684a977cd
Mert <88425310+Periyot@users.noreply.github.com>
2022-02-23 10:17:49 +0300

    Source code

---
+++ webservices.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/Periyot/BodyParser"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/todo", indexHome)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	log.Printf("Open http://localhost:%s in the browser", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	file, _ := ioutil.ReadFile(`data.json`)
+	_, err := fmt.Fprint(w, string(file))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+func indexHome(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	bodyByte, _ := ioutil.ReadAll(r.Body)
+	bodyString := string(bodyByte)
+
+	maps := BodyParser.Parser(bodyString)
+	mapsJSON, _ := json.Marshal(maps)
+	timeNow := time.Now()
+	fmt.Println(`[`, timeNow, `]`, `Body = `, string(mapsJSON))
+	fmt.Fprint(w, string(mapsJSON))
+}
